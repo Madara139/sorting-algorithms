295aaeba6617a2629a13f7164a5a84d1c4d3b3d3
juanh <juanh@LAPTOP-42K13HIQ>
2018-11-04 12:53:10 -0600

    heapsort

--- src/sortingAlgorithms/HeapSort.java
+++ src/sortingAlgorithms/HeapSort.java
@@ -10,23 +10,46 @@ package sortingAlgorithms;
  * @author juanh
  */
 public class HeapSort {
-    private void heapify (int array[], int n){
-        int i, k, aux;
-        boolean band;
-        for(i=2;i<=n;i++){
-            k = i;
-            band = true;
-            while (k>1 && band) {
-                band = false;
-                if (array[k]>array[(k/2)]){
-                    aux = array[(k/2)];
-                    array[(k/2)]= array[k];
-                    array[k]= aux;
-                    k = (k/2);
-                    band = true;
-                }
-            }
-        }
-    }
     
+    private static void heapify(int arr[], int n, int i) { 
+        int mayor = i;  
+        int izq = 2*i + 1;  
+        int der = 2*i + 2; 
+  
+        // si hijo izq es mas grande que raiz
+        if (izq < n && arr[izq] > arr[mayor]) 
+            mayor = izq; 
+  
+        // si hijo derecho es mayor
+        if (der < n && arr[der] > arr[mayor]) 
+            mayor = der; 
+  
+        // si mayor no es raiz 
+        if (mayor != i) 
+        { 
+            int aux = arr[i]; 
+            arr[i] = arr[mayor]; 
+            arr[mayor] = aux; 
+   
+            heapify(arr, n, mayor); 
+        } 
+    } 
+    
+    public static void heapSort(int arr[]) { 
+        int n = arr.length; 
+  
+        // construye monticulo 
+        for (int i = n / 2 - 1; i >= 0; i--) 
+            heapify(arr, n, i); 
+  
+        // deshace monticulo
+        for (int i=n-1; i>=0; i--) 
+        {  
+            int aux = arr[0]; 
+            arr[0] = arr[i]; 
+            arr[i] = aux; 
+  
+            heapify(arr, i, 0); 
+        } 
+    }  
 }
